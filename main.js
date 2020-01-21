de83bd0d24aff9a4828742ab1531a26c8151e549
Mia Dugas <miadugas@outlook.com>
2020-01-21 14:23:19 -0700

    electron-cryptotrack

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 
-
+// Electron QUICKSTART CODE
+// necessary variables for Electron
 const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
+
+//lauch a natrual browser window for CoinMarketCap
 const shell = require('electron').shell
+
+//Collects & sends all the processes out to our windows as needed
 const ipc = require('electron').ipcMain
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+//global reference of the window object that will be created. Will be closed automatically when the JavaScript object is garbage collected.
 let win
 
+//defines the actual application window that will be loaded once a user runs the application. 
 function createWindow () {
-  // Create the browser window.
+
+// Create the browser window.
 win = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
+// and load the index.html of the app via the path.
 win.loadURL(url.format({
+
+// path of template
     pathname: path.join(__dirname, 'src/index.html'),
     protocol: 'file:',
     slashes: true
 }))
 
-  // Open the DevTools.
+// Open the DevTools so we can debug the app, not vital & can comment out if not needed
 win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+// Emitted when the window is closed.
+// Garabge cleanup
 win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+// Dereference the window object, usually you would store windows
+// in an array if the app supports multi window this is the time to delete the corresponding element.
     win = null
 })
 
+//build a simple custom menu with a few submenus
 var menu = Menu.buildFromTemplate([
     {
         label: 'Menu',
@@ -43,8 +52,19 @@ var menu = Menu.buildFromTemplate([
                         shell.openExternal('http://coinmarketcap.com')
                     }
                 },
+                //nice little separator in the submenu items
                 {type: 'separator'},
                 {
+                    //Dev info to website
+                    label: 'Developer Info',
+                    click() {
+                        shell.openExternal('http://miadugas.com')
+                    }
+                },
+                 //nice little separator in the submenu items
+                {type: 'separator'},
+                {
+                  //Shutdown the app
                     label: 'Exit',
                     click() {
                         app.quit()
@@ -67,13 +87,14 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+
+  // on macOS its common for apps & the menu bar to stay active until the user quits with CMD+Q
 if (process.platform !== 'darwin') {
     app.quit()
 }
 })
 
+// final section simply sets win = null when the app has been closed.
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
@@ -82,6 +103,14 @@ if (win === null) {
 }
 })
 
+// ABOVE CODE REQUIRED TO RUN ELECTRON
+// END QUICKSTART CODE
+
+//method used to catch the message from the add.html file & sends back to index.html 
+//calls ipc renderer from our add.html file
 ipc.on('update-notify-value', function(event, arg) {
-win.webContents.send('targetPriceVal', arg)
+win.webContents.
+
+//sends my target price value win which is bound to my index.html file
+send('targetPriceVal', arg)
 })
