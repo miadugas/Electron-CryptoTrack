fe2e3d02952e7b8ddebbdec20f79e62b3e540806
Mia Dugas <miadugas@outlook.com>
2020-01-21 21:34:37 -0700

    electron-cryptotrack

--- src/add.js
+++ src/add.js
@@ -1,5 +1,5 @@
 
-
+//electron required
 const electron = require('electron')
 const path = require('path')
 const remote = electron.remote
@@ -8,6 +8,8 @@ const ipc = electron.ipcRenderer
 const closeBtn = document.getElementById('closeBtn')
 
 closeBtn.addEventListener('click', function(event) {
+    
+     //to close the link that the window is on
     var window = remote.getCurrentWindow();
     window.close()
 })
@@ -17,6 +19,7 @@ const updateBtn = document.getElementById('updateBtn')
 updateBtn.addEventListener('click', function() {
     ipc.send('update-notify-value', document.getElementById('notifyVal').value)
 
+     //close window once submitted
     var window = remote.getCurrentWindow();
     window.close()
 })
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 
-
+//electron required
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+
+// installed as a dev dependency via: 
+// npm install axious --save 
+// now we can make a request
 const axios = require('axios')
 const ipc = electron.ipcRenderer
 
 const notifyBtn = document.getElementById('notifyBtn')
+
+//updating ID's & tags in index.html
 var price = document.querySelector('h1')
 var targetPrice = document.getElementById('targetPrice')
+
+//Desktop notification
 var targetPriceVal
 
 const notification = {
@@ -17,24 +25,44 @@ const notification = {
     icon: path.join(__dirname, '../assets/images/btc.png')
 }
 
+//Axios request to call the function on a time based interval
 function getBTC() {
+
+    //current price of ONE USD bitcoin
+    //API documentaion https://min-api.cryptocompare.com/documentation
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
         .then(res => {
             const cryptos = res.data.BTC.USD
             price.innerHTML = '$'+cryptos.toLocaleString('en')
 
+              //The noticiation function
             if (targetPrice.innerHTML != '' && targetPriceVal < res.data.BTC.USD) {
                 const myNotification = new window.Notification(notification.title, notification)
             }
 
         })
 }
+
+//call to make the function run every 30 seconds
 getBTC()
 setInterval(getBTC, 10000);
 
+//Notify button event listener
 notifyBtn.addEventListener('click', function(event) {
+    
+     //grabs the add.html file
     const modalPath = path.join('file://', __dirname, 'add.html')
-    let win = new BrowserWindow({ frame: false, transparent: true, alwaysOnTop: true, width: 400, height: 200})
+
+    //builds our small window
+    let win = new BrowserWindow({ frame: false, 
+        
+    //make the small window transparent & always on top 
+        transparent: true, alwaysOnTop: true, 
+        
+        //smaller window size
+        width: 400, height: 200})
+    
+    //cleanup files
     win.on('close', function() { win = null })
     win.loadURL(modalPath)
     win.show()
